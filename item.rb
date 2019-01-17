4aa97782daf0212f7e154f734103d0f9d1521c4a
Antonina23 <tone4kawow@gmail.com>
2019-01-17 21:55:18 +0300

    Initialize, Getters and Setters

--- item.rb
+++ item.rb
@@ -1,10 +1,26 @@
 class Item
-    def price
-      rand(100)
+
+    def initialize
+      @price = 30
     end
+
+  # Геттеры:
+    def price
+      @price
+    end
+
+# Сеттеры:
+    def price=(price_value)
+      @price = price_value
+    end
+
 end
 
 item1 = Item.new
-item2 = Item.new
 puts item1.price
-puts item2.price
+
+# вызываем метод price= и задаем значение
+item1.price=(10)
+puts item1.price
+item1.price=(20)
+puts item1.price
